b1e3b77aa8fab5575d7905d07053b07d6498411f
Nick Crossley <ndjc@ncrossley.com>
2018-05-15 12:02:16 -0700

    v0.3.6

--- js/ui/save-html.js
+++ js/ui/save-html.js
@@ -13,8 +13,8 @@ define(
         return {
             show:   function (ui, _conf, _doc, _msg) {
                 msg = _msg, doc = _doc, conf = _conf;
-                var filename = "generated";
-                if (conf.shortName) filename = conf.shortName;
+                var filename = "generated"; // default filename
+                if (conf.shortName) filename = conf.shortName; // but use doc short name if available
                 if (!conf.diffTool) conf.diffTool = "http://www5.aptest.com/standards/htmldiff/htmldiff.pl";
                 var supportsDownload = $("<a href='foo' download='x'>A</a>")[0].download === "x"
                 ,   self = this
